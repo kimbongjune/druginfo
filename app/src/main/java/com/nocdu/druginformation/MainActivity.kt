0d3dee2ac7ce8e120f22abc9ff90119954f03965
admin <nocdu112@naver.com>
2022-07-22 19:52:20 +0900

    2022-07-22 kbj. tablayout apply, main layout design

--- app/src/main/java/com/nocdu/druginformation/MainActivity.kt
+++ app/src/main/java/com/nocdu/druginformation/MainActivity.kt
@@ -11,11 +11,14 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.nocdu.druginformation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle : ActionBarDrawerToggle
+    private lateinit var tabLayout: TabLayout
 
     class MyFragmentPagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity){
         val fragments : List<Fragment>
@@ -35,8 +38,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
-
+        setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
         toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toggle.syncState()
@@ -44,29 +48,47 @@ class MainActivity : AppCompatActivity() {
         val adapter = MyFragmentPagerAdapter(this)
         binding.viewPager.adapter = adapter
         binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-        setContentView(binding.root)
+
+        tabLayout = binding.tabLayout
+        tabLayout.tabRippleColor = null
+        TabLayoutMediator(tabLayout, binding.viewPager){ tab, position ->
+            when(position){
+                0 -> tab.text = "홈"
+                1 -> tab.text = "검색"
+                2 -> tab.text = "알람"
+                3 -> tab.text = "내 정보"
+            }
+        }.attach()
+        setUpTabIcons()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val infrater = menuInflater
-        infrater.inflate(R.menu.menu_main, menu)
-
-        val menuItem = menu?.findItem(R.id.menu_search)
-        val searchView = menuItem?.actionView as SearchView
-
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                Log.e("TAG", "typingText = ${query}")
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return true
-            }
-
-        })
-        return true
+    fun setUpTabIcons(){
+        tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_outline_home_24)
+        tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_baseline_search_24)
+        tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_baseline_access_time_24)
+        tabLayout.getTabAt(3)?.setIcon(R.drawable.ic_outline_info_24)
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        val infrater = menuInflater
+//        infrater.inflate(R.menu.menu_main, menu)
+//
+//        val menuItem = menu?.findItem(R.id.menu_search)
+//        val searchView = menuItem?.actionView as SearchView
+//
+//        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                Log.e("TAG", "typingText = ${query}")
+//                return true
+//            }
+//
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                return true
+//            }
+//
+//        })
+//        return true
+//    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(toggle.onOptionsItemSelected(item)){
